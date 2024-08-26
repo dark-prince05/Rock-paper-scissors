f103dc3979fa627a98497683d598f3bad2e2e87d
Dark-prince <barathraj0504@gmail.com>
2024-08-26 10:55:31 +0530

    add getHumanChoice function which gets input from the user

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let getComputerChoice = () => {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 };
+
+let getHumanChoice = () => {
+  let choice = prompt(`What's your choice "rock", "paper", "scissors"`);
+  return choice;
+};
